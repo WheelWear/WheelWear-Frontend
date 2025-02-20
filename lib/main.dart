9baf6df1343dc4b917657fe65b6958c558428ce8
candosh <csh200310@naver.com>
2025-02-20 17:07:57 +0900

    design/#15/피팅룸 화면 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'features/Auth/login_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+
+Future<void> main() async {
+  // 애플리케이션 시작 전에 dotenv 초기화
+  await dotenv.load(fileName: "assets/config/.env");
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
